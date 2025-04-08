9dd7f3395671b6d4d04c6fd133d632f9835a8460
Hunga1 <awhung99@gmail.com>
2025-04-08 10:23:21 -0400

    Add module federation vite plugin to host and remote

--- react-router-framework-host/vite.config.ts
+++ react-router-framework-host/vite.config.ts
@@ -1,10 +1,30 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { federation } from "@module-federation/vite";
 
 export default defineConfig({
   server: {
     port: 5001
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    target: "chrome89"
+  },
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    federation({
+      name: "host",
+      remotes: {
+        remote: {
+          type: "module",
+          name: "remote",
+          entry: "http://localhost:5002/remoteEntry.js",
+          entryGlobalName: "remote",
+          shareScope: "default",
+        },
+      },
+      filename: "remoteEntry.js",
+      shared: ["react", "react-dom"]
+    })],
 });
--- react-router-framework-remote/vite.config.ts
+++ react-router-framework-remote/vite.config.ts
@@ -1,10 +1,25 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { federation } from "@module-federation/vite";
 
 export default defineConfig({
   server: {
     port: 5002
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    target: "chrome89",
+  },
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    federation({
+      name: "remote",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./remote-app": "./app/root.tsx"
+      },
+      shared: ["react", "react-dom"]
+    }),
+    ],
 });
